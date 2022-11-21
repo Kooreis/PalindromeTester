15418526b407b07111f1176e78bea527318af87a
Kooreis <kooreis.official@gmail.com>
2022-11-21 02:00:00 -0600

    Defined a function to check if a given string is a palindrome.

--- Palindrome_Tester.py
+++ Palindrome_Tester.py
@@ -1,12 +1,2 @@
 def is_palindrome(input_string):
     return input_string == input_string[::-1]
-
-while True:
-    user_input = input("Enter a word to check if it's a palindrome or 'q' to quit: ")
-    if user_input.lower() == 'q':
-        break
-    else:
-        if is_palindrome(user_input):
-            print("It's a palindrome!")
-        else:
-            print("It's not a palindrome.")
