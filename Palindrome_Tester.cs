46fb7bf97252d202b8dbcc3a758da667fb37b56f
Kooreis <kooreis.official@gmail.com>
2022-11-21 02:00:00 -0600

    Initial setup of the program. It prompts the user to enter a word.

--- Palindrome_Tester.cs
+++ Palindrome_Tester.cs
@@ -5,23 +5,5 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("Enter a word: ");
-        string word = Console.ReadLine();
-        string reversedWord = Reverse(word);
-
-        if (word == reversedWord)
-        {
-            Console.WriteLine("The word is a palindrome.");
-        }
-        else
-        {
-            Console.WriteLine("The word is not a palindrome.");
-        }
-    }
-
-    static string Reverse(string s)
-    {
-        char[] charArray = s.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
     }
 }
